d9852e06db7294854de519d453b1af964367ca7a
Malcolm Sparks <malcolm@congreve.com>
2011-04-25 21:31:43 +0100

    Fixed copyright banners to align to project name.

--- test/clj_journal/test/core.clj
+++ test/clj_journal/test/core.clj
@@ -1,13 +1,13 @@
 ;; Copyright 2010 Malcolm Sparks.
 ;;
-;; This file is part of Plugboard.
+;; This file is part of clj-journal.
 ;;
-;; Plugboard is free software: you can redistribute it and/or modify it under the
+;; clj-journal is free software: you can redistribute it and/or modify it under the
 ;; terms of the GNU Affero General Public License as published by the Free
 ;; Software Foundation, either version 3 of the License, or (at your option) any
 ;; later version.
 ;;
-;; Plugboard is distributed in the hope that it will be useful but WITHOUT ANY
+;; clj-journal is distributed in the hope that it will be useful but WITHOUT ANY
 ;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 ;; A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
 ;; details.
